4d73f416d077e47899b25d1087e6a6a7fb7518b6
Alfred Abanto <alfred.jason.abanto@eee.upd.edu.ph>
2022-11-12 15:25:00 +0800

    Added export to excel functionality

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ class Gcash_parser(tk.Tk):
         self.get_files_btn.pack(side=tk.TOP ,expand=True)
 
         self.export_to_xlsx_btn = ttk.Button(self.btn_pnl, text='Export to xlsx')
+        self.export_to_xlsx_btn['command'] = self.export_last_run
         self.export_to_xlsx_btn.pack(side=tk.BOTTOM ,expand=True)
+        self.export_to_xlsx_btn['state'] = 'disable'
 
         #----------------Other Gui elements -----------------
 
@@ -49,7 +51,9 @@ class Gcash_parser(tk.Tk):
     
     # Uses tesserocr to print info
     def get_data_from_files(self):
-        
+        #Clean last run cache
+        self.last_run = []
+
         #Ask for screenshot files to parse
         img_files = tkinter.filedialog.askopenfilenames(initialdir='./')
 
@@ -67,63 +71,98 @@ class Gcash_parser(tk.Tk):
                 raw_list = raw_text.split('\n')
                 
                 #Print only the valuable information to log
+                #Store these in variables
                 self.log_area.insert(tk.INSERT,'\n{}\n'.format(img))
                 date_str = ''
                 amnt = -1
                 ref_str = ''
+
+                #Flags to tell me if the key info has been found
+                fnd_amt = False
+                fnd_ref = False
+                fnd_dat = False
+
+
                 for line in raw_list:
-                    if(line.find('Amount Due PHP') != -1):
-                        amnt = 0
-                    elif(line.find('Amount Paid PHP') != -1):
-                        amnt = 0
-                    elif(line.find('Total PHP') != -1):
-                        amnt = 0
-                    elif(line.find('Ref. No.') != -1):
-                        ref_str = line
-                    else:
-                        for month in self.months:
-                            if(line.find(month) != -1):
-                                date_str = line
-                    
-                    #If Reference number exists, Print and store to data
-                    if(len(ref_str) > 0):
+                    #If amount has not been parsed just yet then look for it first
+                    if(not fnd_amt):
+
+                        if( (line.find('Amount Due PHP') != -1) or \
+                            (line.find('Amount Paid PHP') != -1) or \
+                            (line.find('Total PHP') != -1)  ):
+
+                            # Parse string containing figure : 
+                            # - remove trailing spaces and commas 
+                            # - convert to float
+                            amnt = float( line[line.find('PHP') + len('PHP'):].strip().replace(',',''))
+
+                            #Print for debugging
+                            self.log_area.insert(tk.INSERT, 'Amount : PHP {}'.format(amnt) + '\n')
+                            
+                            #Assert flag
+                            fnd_amt = True
+
+                    elif((line.find('Ref. No.') != -1) and ~fnd_ref):
                         # Parse string containing ref. no. : 
                         # - remove trailing spaces and commas                      
                         ref_str = line[line.find('Ref. No.') + len('Ref. No.'): ].strip().replace(' ','')
-                        self.log_area.insert(tk.INSERT, 'Ref. No. ' + ref_str + '\n')
-                        
-                    
-                    #If Amnt is exists, Print and store to data
-                    if(amnt > -1):
-                        # Parse string containing figure : 
-                        # - remove trailing spaces and commas 
-                        # - convert to float
-                        amnt = float(line[line.find('PHP') + len('PHP'):].strip().replace(',',''))
+                        self.log_area.insert(tk.INSERT, 'Ref. No. : {} \n'.format(ref_str))
+                        fnd_ref = True
 
-                        #Print for debugging
-                        self.log_area.insert(tk.INSERT, 'Amount : PHP '+ str(amnt) + '\n')
-                        
-                    #If date number exists, Print and store to data
-                    if(len(date_str) > 0):
-                        # Parse string containing date : 
-                        # - remove trailing spaces and commas                      
-                        self.log_area.insert(tk.INSERT, 'Date : ' + date_str + '\n')
+                    else:
+                        for month in self.months:
+                            if((line.find(month) != -1) and ~fnd_dat ):
+                                date_str = line
+                                # Parse string containing date : 
+                                # - remove trailing spaces and commas                      
+                                self.log_area.insert(tk.INSERT, 'Date : {}\n'.format(date_str))
+                                fnd_dat = True
                     
-                    #Clean up
-                    amnt = -1
-                    ref_str = ''
-                    date_str = ''
+                        
+                    if(fnd_dat & fnd_amt and fnd_ref):
+                        #Store in last run cache
+                        self.last_run.append([date_str, amnt,ref_str])
+
+                        #Clean up                    
+                        fnd_dat = False
+                        fnd_amt = False
+                        fnd_ref = False
+                        
+                        #Reset flags
+                        amnt = -1
+                        ref_str = ''
+                        date_str = '' 
+
+                #Change state of export to xlsx button after first run
+                if(self.export_to_xlsx_btn['state'] != 'normal'):
+                    self.export_to_xlsx_btn['state'] = 'enable'
+                      
 
                 self.log_area.insert(tk.INSERT,'\n--------------------------\n')
                 
             
         self.log_area.insert(tk.INSERT,'Done! '.format(len(img_files)))
-
-                    
-
         print('Done')
                         
-                    
+    def export_last_run(self):
+        output_filename =  tkinter.filedialog.asksaveasfilename( defaultextension='.xlsx')
+        print(output_filename)
+        # Create a workbook and add a worksheet.
+        workbook = xlsxwriter.Workbook('Test_out.xlsx')
+        worksheet = workbook.add_worksheet()
+
+        # Start from the first cell. Rows and columns are zero indexed.
+        row = 0
+        col = 0
+
+        # Iterate over the data and write it out row by row.
+        for date, amnt, ref in (self.last_run):
+            worksheet.write(row, col,     date)
+            worksheet.write(row, col + 1, amnt)
+            worksheet.write(row, col + 2, ref)
+            row += 1
+
+        workbook.close()
 
 
 if __name__ == "__main__":
